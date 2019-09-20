bb1ddbbce56a0364e2fd6a806cd32f4200979dbd
Shankar <212604198@logon.ds.ge.com>
2019-09-20 11:16:34 +0530

    Initial checkin of angular elements code

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import {createCustomElement} from '@angular/elements';
+
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  entryComponents: [HelloWorldComponent],
+  providers: [],
+})
+export class AppModule { 
+  constructor(injector: Injector) {
+    const custom = createCustomElement(HelloWorldComponent, {injector: injector});
+    customElements.define('app-hello-world', custom);
+  }
+  ngDoBootstrap() {
+    
+  }
+}
